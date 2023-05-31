ebfb29e86b82b1a614600f28e95eac9f7f99ea41
tanya-kostecki <tanya.pavlova1294@gmail.com>
2023-05-31 18:51:41 +0200

    Выносит форму входа в отдельный компонент

--- index.js
+++ index.js
@@ -4,10 +4,12 @@
     //  * Сделать форму входа динамической (+)
     //  * Отрефакторить приложение на модули
     //      * api (+)
-    //      * ... TODO
+    //      * вытащить логин компонент в отдельный модуль (+)
+    //      * вытащить компонент списка и форму добавления в отдельный модуль (+)
     // 2. Реализовать форму регистрации
 
     import { addTodo, deleteTodo, getTodos } from "./api.js";
+    import { renderLoginComponent } from './components/login-component.js'
 
     let tasks = [];
 
@@ -27,27 +29,14 @@
       const appEl = document.getElementById("app");
       
       if(!token) {
-        const appHtml = `
-                <h1>Список задач</h1>
-                <div class="form">
-                <h3 class="form-title">Форма входа</h3>
-                <div class="form-row">
-                    Логин
-                    <input type="text" id="login-input" class="input" />
-                    <br />
-                    Пароль
-                    <input type="text" id="login-input" class="input" />
-                </div>
-                <br />
-                <button class="button" id="login-button">Войти</button>
-                </div>
-        `;
-        appEl.innerHTML = appHtml;
+        renderLoginComponent({
+          appEl, 
+          setToken: (newToken) => {
+            token = newToken;
+          },
+          fetchTodosAndRender,
+       });
 
-        document.getElementById('login-button').addEventListener('click', () => {
-          token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
-          fetchTodosAndRender();
-        })
         return;
       }
       
