5ca0cb389b3474648b70213a651746426d6597a3
tanya-kostecki <tanya.pavlova1294@gmail.com>
2023-05-31 19:51:21 +0200

    Оживляет форму регистрации

--- api.js
+++ api.js
@@ -1,58 +1,61 @@
 const host = "https://webdev-hw-api.vercel.app/api/v2/todos";
 
 export function getTodos({ token }) {
-    return fetch(host, {
-        method: "GET",
-        headers: {
-          Authorization: token,
-        },
-      })
-        .then((response) => {
-          if (response.status === 401) {
-            // token = prompt("Введите верный пароль");
-            // fetchTodosAndRender();
-            throw new Error("Нет авторизации");
-          }
-          return response.json();
-        });
+  return fetch(host, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      if (response.status === 401) {
+        // token = prompt("Введите верный пароль");
+        // fetchTodosAndRender();
+        throw new Error("Нет авторизации");
+      }
+      return response.json();
+    });
 };
 
 export function deleteTodo({ token, id }) {
-    return fetch("https://webdev-hw-api.vercel.app/api/todos/" + id, {
-        method: "DELETE",
-        headers: {
-          Authorization: token,
-        },
-      })
-        .then((response) => {
-          return response.json();
-        });
+  return fetch("https://webdev-hw-api.vercel.app/api/todos/" + id, {
+    method: "DELETE",
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      return response.json();
+    });
 };
 
 export function addTodo({ text, token }) {
-    return fetch(host, {
-        method: "POST",
-        body: JSON.stringify({
-          text,
-        }),
-        headers: {
-          Authorization: token,
-        },
-      })
-        .then((response) => {
-          return response.json();
-        })
+  return fetch(host, {
+    method: "POST",
+    body: JSON.stringify({
+      text,
+    }),
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
 }
 
-export function login({ login, password }) {
+export function loginUser({ login, password }) {
   return fetch('https://wedev-api.sky.pro/api/user/login', {
-      method: "POST",
-      body: JSON.stringify({
-        login,
-        password
-      }),
+    method: "POST",
+    body: JSON.stringify({
+      login,
+      password
+    }),
+  })
+    .then((response) => {
+      if(response.status === 400) {
+        throw new Error('Неверный логин или пароль')
+      }
+      return response.json();
     })
-      .then((response) => {
-        return response.json();
-      })
 }
--- components/login-component.js
+++ components/login-component.js
@@ -1,4 +1,4 @@
-import { login } from '../api.js'
+import { loginUser } from '../api.js'
 export function renderLoginComponent({ appEl, setToken, fetchTodosAndRender }) {
     const appHtml = `
         <h1>Список задач</h1>
@@ -9,7 +9,7 @@ export function renderLoginComponent({ appEl, setToken, fetchTodosAndRender }) {
             <input type="text" id="login-input" class="input" />
             <br />
             Пароль
-            <input type="text" id="login-input" class="input" />
+            <input type="password" id="password-input" class="input" />
         </div>
         <br />
         <button class="button" id="login-button">Войти</button>
@@ -19,13 +19,30 @@ export function renderLoginComponent({ appEl, setToken, fetchTodosAndRender }) {
     appEl.innerHTML = appHtml;
 
     document.getElementById('login-button').addEventListener('click', () => {
-        login({
-            login: "admin",
-            password: "admin",
+        const login = document.getElementById('login-input').value;
+        const password = document.getElementById('password-input').value;
+
+        if (!login) {
+            alert('Введите логин');
+            return;
+        }
+
+        if (!password) {
+            alert('Введите пароль');
+            return;
+        }
+
+        loginUser({
+            login: login,
+            password: password,
         })
-        .then((user) => {
-            setToken(`Bearer ${user.user.token}`);
-            fetchTodosAndRender();
-        })
+            .then((user) => {
+                setToken(`Bearer ${user.user.token}`);
+                fetchTodosAndRender();
+            })
+            .catch((error) => {
+                //TODO: Выводить alert красиво 
+                alert(error.message);
+            })
     });
 }
